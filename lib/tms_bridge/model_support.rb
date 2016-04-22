bfb48fc7785dde1dd8499f349ece53aa99e23d3d
Erich L Timkar <erich@teamdriveaway.com>
2016-04-22 10:40:04 -0500

    Adds support for attribute_aliases to model_support

--- lib/tms_bridge/model_support.rb
+++ lib/tms_bridge/model_support.rb
@@ -1,7 +1,9 @@
 module TmsBridge
   module ModelSupport
     def published_attribute_names
-      self.column_names - %w{id created_at updated_at}
+      _attributes_names = self.column_names - %w{id created_at updated_at}
+      _attributes_names+=self.attribute_aliases.keys
+      return _attributes_names.compact.map(&:to_s).uniq
     end
   end
 end
--- spec/tms_bridge/model_support_spec.rb
+++ spec/tms_bridge/model_support_spec.rb
@@ -4,20 +4,25 @@ require File.expand_path(File.dirname(__FILE__) + '/mocks')
 
 describe TmsBridge::ModelSupport do
   describe "published_attributes" do
-    it "should not include 'id'" do
-      MockModel.published_attribute_names.should_not include('id')      
-    end
+    # it "should not include 'id'" do
+    #   MockModel.published_attribute_names.should_not include('id')
+    # end
+    #
+    # it "should not include 'created_at'" do
+    #   MockModel.published_attribute_names.should_not include('created_at')
+    # end
+    #
+    # it "should not include 'updated_at" do
+    #   MockModel.published_attribute_names.should_not include('updated_at')
+    # end
+    #
+    # it "should include items declared in column_names" do
+    #   MockModel.published_attribute_names.should include('some_key')
+    # end
     
-    it "should not include 'created_at'" do
-      MockModel.published_attribute_names.should_not include('created_at')
-    end
-    
-    it "should not include 'updated_at" do
-      MockModel.published_attribute_names.should_not include('updated_at')
-    end
-    
-    it "should include items declared in column_names" do
-      MockModel.published_attribute_names.should include('some_key')
+    it "should include alias_attributes" do
+      MockModel.should_receive(:attribute_aliases){{"alias_key"=>"some_key"}}
+      MockModel.published_attribute_names.should include('alias_key')
     end
   end
 end
